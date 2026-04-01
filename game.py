6dc365189bc58939461ba2fe07f4b7a3fe183fff
Süleyman Sarokhan <suleymansarokhan47@gmail.com>
2026-04-01 14:23:12 +0200

    arbeitstag 1

--- game.py
+++ game.py
@@ -1,31 +1,13 @@
-import customtkinter as ctk
 import random as rnd
-
-app = ctk.CTk()
-app.title("Wörtre")
-
-var_input = ""
-level_length = 3
-
-word_list = [
-    "AAL", "ABO", "AKT", "ALM", "AMT", "ART", "AST", "AUF", "AUS", "BAD", 
-    "BAU", "BEI", "BIT", "BOX", "BUH", "EHE", "EIS", "ELF", "ERZ", "FAN", 
-    "FAX", "FEE", "GEL", "GEN", "GUT", "HAI", "HOF", "HUT", "ICH", "IHM", 
-    "IHN", "IHR", "INN", "IST", "JOB", "KAI", "KID", "KUR", "LAB", "LOB", 
-    "LOG", "LOS", "MAI", "MAL", "MAU", "MET", "MIX", "MUT", "NAH", "NEU", 
-    "NIE", "NOT", "NUN", "OFT", "OHR", "OST", "RAD", "RAT", "RAU", "REH", 
-    "ROH", "ROT", "RUF", "SAU", "SEE", "SET", "SIE", "SKI", "SOL", "TAG", 
-    "TAT", "TEE", "TOD", "TON", "TOR", "TOT", "TUN", "UHR", "UND", "UNS", 
-    "UWE", "VON", "VOR", "WAL", "WAS", "WEG", "WEM", "WEN", "WER", "WIE", 
-    "WIR", "WUT", "ZUG"]
-
-correct_word = rnd.choice(word_list)
+import game_style
 
 def handle_key(event):
     # event.keysym enthält den Namen der Taste (z.B. 'a', 'Return', 'BackSpace')
     key = event.keysym
     global var_input 
     global level_length
+
+    game_style.buildLevel(level_length)
 
     if len(var_input) < level_length or (key == "BackSpace" or key == "Return"):
         if key == "Return":
@@ -52,8 +34,26 @@ def check_input(user_input):
     else:
         return False
 
+game_style.styleApp()
+
+var_input = ""
+level_length = 3
+
+word_list = [
+    "AAL", "ABO", "AKT", "ALM", "AMT", "ART", "AST", "AUF", "AUS", "BAD", 
+    "BAU", "BEI", "BIT", "BOX", "BUH", "EHE", "EIS", "ELF", "ERZ", "FAN", 
+    "FAX", "FEE", "GEL", "GEN", "GUT", "HAI", "HOF", "HUT", "ICH", "IHM", 
+    "IHN", "IHR", "INN", "IST", "JOB", "KAI", "KID", "KUR", "LAB", "LOB", 
+    "LOG", "LOS", "MAI", "MAL", "MAU", "MET", "MIX", "MUT", "NAH", "NEU", 
+    "NIE", "NOT", "NUN", "OFT", "OHR", "OST", "RAD", "RAT", "RAU", "REH", 
+    "ROH", "ROT", "RUF", "SAU", "SEE", "SET", "SIE", "SKI", "SOL", "TAG", 
+    "TAT", "TEE", "TOD", "TON", "TOR", "TOT", "TUN", "UHR", "UND", "UNS", 
+    "UWE", "VON", "VOR", "WAL", "WAS", "WEG", "WEM", "WEN", "WER", "WIE", 
+    "WIR", "WUT", "ZUG"]
+
+correct_word = rnd.choice(word_list)
 
 # Bindet alle Tastendruck-Events an die Funktion
-app.bind("<Key>", handle_key)
+game_style.app.bind("<Key>", handle_key)
 
-app.mainloop()
+game_style.app.mainloop()
